a95775b721eb73eb9d871ebf028c47e7dc07d686
jaykingvibe <jaykingvibe@gmail.com>
2022-09-08 20:25:47 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - entry point
  *
- * Description: using the main function 
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
+ * Return: always 0
  */
 int main(void)
 {
