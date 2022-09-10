27c25106e4e8aa079f532002610ab6879c17333e
jaykingvibe <jaykingvibe@gmail.com>
2022-09-09 21:59:28 -0700

    task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-        char a;
+	char a;
 
 	for (a = 'a'; a <= 'z' ; a++)
-        putchar(a);
+	putchar(a);
 	for (a = 'A'; a <= 'Z'; a++)
-        putchar(a);
+	putchar(a);
 	putchar('\n');
 	return (0);
 }
