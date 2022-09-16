c7b27a07d465313d0629664ea5e9ef08104b11c8
jaykingvibe <jaykingvibe@gmail.com>
2022-09-16 02:29:49 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+/**
+ *
+ * print_alphabet - this program prints alphabet
+ *
+ *
+ *
+ * Return: 0
+ *
+ */
+
+void print_alphabet(void)
+{
+		char alfa = 'a';
+
+		while (alfa <= 'z')
+
+		{
+			write(1, &alfa, 1);
+			alfa++;
+		}
+		write(1, "\n", 1);
+}
